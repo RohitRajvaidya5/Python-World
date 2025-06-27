001c676a650f3216f6e2194bef45dd7e38e1eef3
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2025-06-27 18:09:40 +0530

    Added Exceptional Handling

--- Projects/Quiz App ( Console Base )/script.py
+++ Projects/Quiz App ( Console Base )/script.py
@@ -1,29 +1,56 @@
 import json
 
 # Load questions from the JSON file
-with open('question.json', 'r') as questions:
-    data = json.load(questions)
+try:
+
+    with open('question.json', 'r') as questions:
+        data = json.load(questions)
+
+except FileNotFoundError:
+
+    print("Question file not found.")
+
+    exit()
+
+except json.JSONDecodeError:
+    
+    print("JSON format is incorrect")
+
+    exit()
 
 count = 0
 
 # Loop through each question
 for i in range(len(data)):
-    print(data[i]['question'])
 
-    # Print each option on a new line
-    for option in data[i]['options']:
-        print(option)
+    try:
+        print(data[i]['question'])
 
-    # Take user input
-    ans = input("Enter the correct option: ")
+        # Print each option on a new line
+        for option in data[i]['options']:
+            print(option)
 
-    # Check answer
-    if ans == data[i]['answer']:
-        print("Correct answer, let's go for the next!\n")
-        count = count + 1
-    else:
-        print("Incorrect answer, one point taken.\n")
-        count = count - 1
+        # Take user input
+        ans = input("Enter the correct option: ").upper()
+
+        # Check answer
+        if ans == data[i]['answer']:
+            print("Correct answer, let's go for the next!\n")
+            count = count + 1
+        else:
+            print(f"Incorrect answer, one point taken.\nCorrect Answer is '{data[i]['answer']}' ")
+            if count == 0:
+                count = 0
+            else:
+                count = count - 1
+            
+    except KeyError as e:
+        print(f"Missing key in question data {e}")
+        continue
+
+    except Exception as e:
+        print(f"Unexpected Error : {e}")
+        continue
 
 # Final score
 print(f"Total Score: {count}")
