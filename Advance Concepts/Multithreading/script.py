f8d071f7506879677dc20000def513565cc5b0df
Rohit Rajvaidya <109543331+RohitRajvaidya5@users.noreply.github.com>
2025-07-10 00:27:52 +0530

    Update script.py

--- Advance Concepts/Multithreading/script.py
+++ Advance Concepts/Multithreading/script.py
@@ -1,15 +1,3 @@
-'''
-Multithreading :
-
-Multithreading is a technique where multiple threads (sub-routines) run concurrently within a single process. This helps improve performance when:
-
-You have tasks that are waiting, like reading files, waiting for network/data, or sleeping.
-
-You want to make your program more responsive (e.g., running a progress bar while downloading a file).
-
-'''
-
-
 import threading
 import time
 
